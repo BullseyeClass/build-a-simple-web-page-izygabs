e2cca8558c10774cf420ad3fda998d294b7f4b2c
izygabs <80975143+izygabs@users.noreply.github.com>
2023-05-04 00:20:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// function changeText(result) {
+//   result.innerHTML = "Hello, World!";
+// }
+
+// function displayAlert() {
+//   document.getElementById("result").innerHTML = "Button clicked!";
+// }
+
+function displayAlert() {
+  alert("Hello, World!");
+  let result = document.getElementById("result");
+  result.innerHTML = "Button clicked!";
+}
